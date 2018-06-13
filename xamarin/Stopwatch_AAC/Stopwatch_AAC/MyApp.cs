00db4c354ac66166e85019aee31989ffc92951f2
Thomas Künneth <thomas.kuenneth@mathema.de>
2018-06-13 08:34:21 +0200

    added example for an application lifecycle observer

---
+++ xamarin/Stopwatch_AAC/Stopwatch_AAC/MyApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.App;
+using Android.Arch.Lifecycle;
+using Android.Util;
+using Java.Interop;
+
+namespace Stopwatch_AAC
+{
+    [Application]
+    public class MyApp : Application, ILifecycleObserver
+    {
+        const string TAG = "MyApp";
+
+        public MyApp(IntPtr handle, Android.Runtime.JniHandleOwnership ownerShip) : base(handle, ownerShip)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            ProcessLifecycleOwner.Get().Lifecycle.AddObserver(this);
+        }
+
+        [Lifecycle.Event.OnStop]
+        [Export]
+        public void Stopped()
+        {
+            Log.Debug(TAG, "App entered background state.");
+        }
+
+        [Lifecycle.Event.OnStart]
+        [Export]
+        public void Started()
+        {
+            Log.Debug(TAG, "App entered foreground state.");
+        }
+    }
+}
