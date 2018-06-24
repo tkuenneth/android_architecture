610539b7bb5eebe456ec3a689338937cc20499d9
Thomas Kuenneth <thomas@thomaskuenneth.com>
2018-06-24 13:06:02 +0200

    Xamarin version witout architecture components added

---
+++ xamarin/Stopwatch/Stopwatch/MainActivity.cs
@@ -0,0 +1,153 @@
+﻿using Android.App;
+using Android.OS;
+using Java.Text;
+using Java.Util;
+using System;
+using Android.Support.V7.App;
+using Android.Widget;
+using Java.Lang;
+using Android.Views;
+
+namespace Stopwatch
+{
+    [Activity(Label = "Stopwatch", Exported = true, MainLauncher = true)]
+    public class StopwatchActivity : AppCompatActivity
+    {
+        const string KEY_DIFF = "diff";
+        const string KEY_RUNNING = "running";
+
+        readonly DateFormat format;
+        Timer timer;
+        TimerTask timerTask;
+        TextView time;
+        Button startStop;
+        Button reset;
+        long started;
+        bool isRunning;
+        long diff;
+
+        public StopwatchActivity()
+        {
+            format = new SimpleDateFormat("HH:mm:ss:SSS",
+                    Locale.Us)
+            {
+                TimeZone = Java.Util.TimeZone.GetTimeZone("UTC")
+            };
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            time = FindViewById<TextView>(Resource.Id.time);
+            startStop = FindViewById<Button>(Resource.Id.start_stop);
+            startStop.SetOnClickListener(new MyOnClickListener(() =>
+            {
+                isRunning = !isRunning;
+                if (isRunning)
+                {
+                    ScheduleAtFixedRate();
+                }
+                else
+                {
+                    timerTask.Cancel();
+                }
+                UpdateUI();
+            }));
+            reset = FindViewById<Button>(Resource.Id.reset);
+            reset.SetOnClickListener(new MyOnClickListener(() => { ClearTime(); }));
+            if (savedInstanceState != null)
+            {
+                GetValuesFromBundle(savedInstanceState);
+                SetTime();
+            }
+            else
+            {
+                isRunning = false;
+                ClearTime();
+            }
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            outState.PutLong(KEY_DIFF, diff);
+            outState.PutBoolean(KEY_RUNNING, isRunning);
+        }
+
+        protected override void OnRestoreInstanceState(Bundle savedInstanceState)
+        {
+            base.OnRestoreInstanceState(savedInstanceState);
+            GetValuesFromBundle(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            timer = new Timer();
+            UpdateUI();
+            if (isRunning)
+            {
+                ScheduleAtFixedRate();
+            }
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            timer.Cancel();
+        }
+
+        private void ClearTime()
+        {
+            time.SetText(Resource.String.cleared);
+            diff = 0;
+        }
+
+        private void UpdateUI()
+        {
+            startStop.SetText(isRunning ? Resource.String.stop : Resource.String.start);
+            reset.Enabled = !isRunning;
+        }
+
+        private void GetValuesFromBundle(Bundle b)
+        {
+            diff = b.GetLong(KEY_DIFF);
+            isRunning = b.GetBoolean(KEY_RUNNING);
+        }
+
+        private void SetTime()
+        {
+            time.Text = format.Format(new Date(diff));
+        }
+
+        private void ScheduleAtFixedRate()
+        {
+            started = JavaSystem.CurrentTimeMillis() - diff;
+            timerTask = new MyTimerTask(() =>
+            {
+                diff = JavaSystem.CurrentTimeMillis() - started;
+                RunOnUiThread(() =>
+                {
+                    SetTime();
+                });
+            });
+            timer.ScheduleAtFixedRate(timerTask, 0, 200);
+        }
+    }
+
+    public sealed class MyOnClickListener : Java.Lang.Object, View.IOnClickListener
+    {
+        readonly Action action;
+
+        public MyOnClickListener(Action action)
+        {
+            this.action = action;
+        }
+
+        void View.IOnClickListener.OnClick(View v)
+        {
+            action();
+        }
+    }
+}
---
+++ xamarin/Stopwatch/Stopwatch/MyTimerTask.cs
@@ -0,0 +1,20 @@
+﻿using Java.Util;
+using System;
+
+namespace Stopwatch
+{
+    public class MyTimerTask : TimerTask
+    {
+        readonly Action action;
+
+        public MyTimerTask(Action action)
+        {
+            this.action = action;
+        }
+
+        public override void Run()
+        {
+            action();
+        }
+    }
+}
